757c833e70705e78aad8b7dd50b347ca4e0bfe44
Resul Takak <resultakak@gmail.com>
2021-08-17 00:49:37 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# (>_ resultakak@gmail.com)
+
+https://github.com/resultakak/laravel-contact-form-api.git
+
+cd ./laravel-contact-form-api
+
+cp .env.example .env
+
+chmod +x setup.sh
+
+docker build -t resultakak/php:mavi ./build/php
+
+docker-compose up -d
+
+docker-compose exec laravel.test sh setup.sh
