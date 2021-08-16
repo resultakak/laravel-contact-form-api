3a166340b764067648e9bd55d87a15b4b7393008
Resul Takak <resultakak@gmail.com>
2021-08-16 23:02:59 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,50 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::post('/v1/token/register', function (Request $request) {
+    $request->validate(
+        [
+            'name'     => 'required|min:2|max:100',
+            'email'    => 'required|email|unique:users|min:6|max:254',
+            'password' => 'required|min:8|max:12'
+        ]
+    );
+
+    $user = User::where('email', $request->email)->first();
+
+    if ($user) {
+        return response()->json(['message' => 'Email already exists'], 400);
+    }
+
+    $user = User::create(
+        [
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => bcrypt($request->password),
+        ]
+    );
+
+    return ['token' => $user->createToken($request->email)->plainTextToken];
+});
+
+Route::post('/v1/token/create', function (Request $request) {
+    $request->validate(
+        [
+            'email'    => 'required|email|min:6|max:254',
+            'password' => 'required|min:8|max:12'
+        ]
+    );
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        return response()->json(['message' => 'The credentials are incorrect'], 400);
+    }
+
+    return ['token' => $user->createToken($request->email)->plainTextToken];
+});
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
 });
